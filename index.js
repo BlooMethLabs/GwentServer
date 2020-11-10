43232bcfe756a456ce10458f1cbb2038bc2c9fad
BlooMethLabs <bmca620@gmail.com>
2020-11-10 22:32:51 +0000

    Comment out other setHeader as only last one is effective

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
+  // res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
 
   // Request methods you wish to allow
   res.setHeader(
